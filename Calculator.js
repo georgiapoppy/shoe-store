424a84b8a1b3e71e6d15bd053300a528ab89517a
Georgia Mason <georgiapoppym@gmail.com>
2024-04-06 19:56:17 +1030

    changes

--- Calculator.js
+++ Calculator.js
@@ -1,21 +1,21 @@
 var operator = 0;
 function add() {
-document.getElementById("addB").style.backgroundcolor = "darkgrey";
+document.getElementById("addB").style.backgroundColor = "darkslategray";
 document.getElementById("addB").style.color = "white";
 operator = 1;
 }
 function sub() {
-document.getElementById("subB").style.backgroundcolor = "darkgrey";
+document.getElementById("subB").style.backgroundColor = "darkslategray";
 document.getElementById("subB").style.color = "white";
 operator = 2;
 }
 function times() {
-document.getElementById("timesB").style.backgroundcolor = "darkgrey";
+document.getElementById("timesB").style.backgroundColor = "darkslategray";
 document.getElementById("timesB").style.color = "white";
 operator = 3;
 }
 function divide() {
-document.getElementById("divideB").style.backgroundcolor = "darkgrey";
+document.getElementById("divideB").style.backgroundColor = "darkslategray";
 document.getElementById("divideB").style.color = "white";
 operator = 4;
 }
